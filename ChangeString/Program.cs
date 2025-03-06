f4baf65edcfb5772968c9dfd34cad32e7388e050
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-06 00:04:48 +0000

    Begin Change str

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO.Compression;
+using System.Net.Sockets;
 
 namespace ChangeString
 {
@@ -6,7 +8,28 @@ namespace ChangeString
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Escreva algo: ");
+            string f = Console.ReadLine();
+
+            Console.WriteLine("Escreva uma letra: ");
+            string l = Console.ReadLine();
+            Convert.ToChar(l);
+            
+
+
+            foreach (char c in  f)
+            {
+                if ( l == "") 
+                {
+                    Console.WriteLine("x");
+                }
+                Console.WriteLine(f);
+                break;
+            }
+
+           
+            
+
         }
     }
 }
