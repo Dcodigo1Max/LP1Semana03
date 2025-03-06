09fb440c7a0fc9a01cfec5965f36ef5a60aecdc9
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-06 14:04:43 +0000

    Exer 4 inicio

---
+++ HeroPerk/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HeroPerk
+{
+    public class Program
+    {
+
+           [Flags]
+           enum Perks
+           {
+            WarpShift =1 << 0,
+                Stealh =1 << 1,
+                AutoHeal =1 << 2,
+                DoubleJump=1 << 3,
+
+           }
+        private static void Main(string[] args)
+        {
+
+          string[] comandos = new string {a, s, d, w};
+           
+           
+           
+           
+           
+           
+           
+        }
+    }
+}
