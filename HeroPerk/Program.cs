0ff647a962c940da514b1c7d3af7b0d8bd793e67
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-07 16:52:59 +0000

    MyPerksDone

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -10,7 +10,7 @@ namespace HeroPerk
            enum Perks
            {
               WarpShift =1 << 0,
-              Stealh =1 << 1,
+              Stealth =1 << 1,
               AutoHeal =1 << 2,
               DoubleJump=1 << 3,
 
@@ -18,55 +18,64 @@ namespace HeroPerk
         private static void Main(string[] args)
         {
 
-          Perks myperks = Perks.WarpShift | Perks.Stealh | Perks.AutoHeal | Perks.DoubleJump;
-
-          myperks ^=Perks.WarpShift;
-
-          myperks ^=Perks.Stealh;
-          
-          myperks ^=Perks.AutoHeal;
-          
-          myperks ^=Perks.DoubleJump;
-
-          
-          
+          Perks myperks = 0;
           string part = "";
 
-          for (int u =0; u <args.Length; u ++)
+            for (int u =0; u <args.Length; u ++)
           {
             
-            part = args[u];
+            part += args[u];
           }
 
-          myperks =0;
 
-          foreach(string u in myperks)
+          //Perks myperks = Perks.WarpShift | Perks.Stealth | Perks.AutoHeal | Perks.DoubleJump;
+
+         
+
+
+
+          foreach(Char argumento in part)
           {
-
-          }
-
-          Console.WriteLine(myperks);
-
-
-
-          /*
-
-          if ()
-          {
-            Console.WriteLine("!Not gonna make it!");
-          }
-
-          if ()
-          {
-              Console.WriteLine("!Silent jumper!");
-          }
-           */
-           
-           
-           
-           
-           
-           
+             if (argumento.Equals('w') == true)
+                {
+                  myperks ^= Perks.WarpShift;
+                }
+                 else if (argumento.Equals('s') == true)
+                {
+                    myperks ^= Perks.Stealth;
+                }
+                else if (argumento.Equals('a') == true)
+                {
+                    myperks ^= Perks.AutoHeal;
+                }
+                else if (argumento.Equals('d') == true)
+                {
+                    myperks ^= Perks.DoubleJump;
+                }
+                else
+                {
+                    Console.WriteLine("!Unknown perk!");
+                    return;
+                }
+            }
+              if(myperks.HasFlag(Perks.AutoHeal) | myperks.HasFlag(Perks.Stealth) | myperks.HasFlag(Perks.DoubleJump) |
+                myperks.HasFlag(Perks.WarpShift))
+                {
+                    Console.WriteLine(myperks);
+                }
+            else
+            {
+                Console.WriteLine("!No perks at all!");
+            }
+            if (myperks.HasFlag(Perks.Stealth) & myperks.HasFlag(
+            Perks.DoubleJump))
+            {
+                Console.WriteLine("!Silent jumper!");
+            }
+            if ((myperks & Perks.AutoHeal) != Perks.AutoHeal)
+            {
+                Console.WriteLine("!Not gonna make it!");
+            }           
         }
     }
 }
