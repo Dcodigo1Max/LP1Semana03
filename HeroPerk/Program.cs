8af3e8a25277e06e7f6cff4e5addb6499d146c3b
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-07 00:09:21 +0000

    Almost done Perks

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security;
 
 namespace HeroPerk
 {
@@ -8,17 +9,58 @@ namespace HeroPerk
            [Flags]
            enum Perks
            {
-            WarpShift =1 << 0,
-                Stealh =1 << 1,
-                AutoHeal =1 << 2,
-                DoubleJump=1 << 3,
+              WarpShift =1 << 0,
+              Stealh =1 << 1,
+              AutoHeal =1 << 2,
+              DoubleJump=1 << 3,
 
            }
         private static void Main(string[] args)
         {
 
-          string[] comandos = new string {a, s, d, w};
-           
+          Perks myperks = Perks.WarpShift | Perks.Stealh | Perks.AutoHeal | Perks.DoubleJump;
+
+          myperks ^=Perks.WarpShift;
+
+          myperks ^=Perks.Stealh;
+          
+          myperks ^=Perks.AutoHeal;
+          
+          myperks ^=Perks.DoubleJump;
+
+          
+          
+          string part = "";
+
+          for (int u =0; u <args.Length; u ++)
+          {
+            
+            part = args[u];
+          }
+
+          myperks =0;
+
+          foreach(string u in myperks)
+          {
+
+          }
+
+          Console.WriteLine(myperks);
+
+
+
+          /*
+
+          if ()
+          {
+            Console.WriteLine("!Not gonna make it!");
+          }
+
+          if ()
+          {
+              Console.WriteLine("!Silent jumper!");
+          }
+           */
            
            
            
