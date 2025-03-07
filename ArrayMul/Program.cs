093edfb4db00a674d979b8f808663a3ac3c8466f
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-07 10:43:23 +0000

    ArrayMul Start

---
+++ ArrayMul/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayMul
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+           float [,] A = new float [2,2];
+           float [,] b = new float [2,1];
+
+           //A [0,0] = "";
+           //b [0,0] = "";
+
+
+           /* for(float i = 0; i<A.Length; i++)
+            {
+                for(float j = 0; j < A[i].lenght; j++)
+                {
+                    Console.WriteLine(A[i][j] + " ");
+                }
+                Console.WriteLine("| "+ A + " |");
+            }
+            */
+
+
+        }
+    }
+}
